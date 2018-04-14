926143b6c1e7774035115a6cbed0d5d3c38f9ebf
Priyanka Jain <priyankajain.jain07@gmail.com>
2018-04-14 16:34:45 +0530

    Added contact list component and created module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ContactListModule} from "./contact-list/contact-list.module";
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContactListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
